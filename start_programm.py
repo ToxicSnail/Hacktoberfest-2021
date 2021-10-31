7a88a6a0763897b8b2ca2c607161eeed23aa387e
Kirill <59337566+kirill010403@users.noreply.github.com>
2021-10-31 11:41:14 +0300

    Update start_programm.py

--- start_programm.py
+++ start_programm.py
@@ -2,7 +2,6 @@ import os
 
 n = input('Какую монету вы хотите видеть?\n')
 if n == 'eth':
-    os.system('test_eth.py')
+    os.system('run_eth.py')
 elif n == 'hot':
     os.system('run_hot.py')
-    
